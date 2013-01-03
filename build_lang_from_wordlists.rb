f1f349a178f5fab9d196f800297a17c8e1d78798
Annika Sirapandji <annika.sirapandji@xing.com>
2013-01-03 14:36:15 +0100

    added wordlists for arabic, chinese, czech, danish, finnish, greek, hebrew, hungarian, korean, norwegian, polish, romanian, turkish and created *.lang files

--- build_lang_from_wordlists.rb
+++ build_lang_from_wordlists.rb
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 # Builds all of the word lists in ./wordlists/ into filter files in ./lang/
-
+$: << File.dirname(__FILE__)
 require 'lib/whatlanguage'
 
 languages_folder = File.join(File.dirname(__FILE__), "lang")
@@ -7,6 +8,7 @@ wordlists_folder = File.join(File.dirname(__FILE__), "wordlists")
 
 Dir.entries(wordlists_folder).grep(/\w/).each do |lang|
   next if lang == 'generators'
+  next if lang == '.DS_Store'
   puts "Doing #{lang}"
   filter = WhatLanguage.filter_from_dictionary(File.join(wordlists_folder, lang))
   File.open(File.join(languages_folder, lang + ".lang"), 'wb') { |f| f.write filter.dump }
